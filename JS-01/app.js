0b273d105104a48ae7395fb7a10e151fb890d5b5
Muhammad Yasir <optimisticyasir@outlook.com>
2023-08-06 19:57:39 +0500

    added JS Assignments

---
+++ JS-01/app.js
@@ -0,0 +1,103 @@
+// Chapter 01 Assignment
+
+// 1. Write a script to greet your website visitor using JS alert
+// box.
+
+// alert("Welcome! Hi there! Nice to see you.");
+
+// 2.
+
+// alert("Error! Please enter a valid password.");
+
+// 3.
+
+// alert("Welcome to JS Land... \nHappy Coding!");
+
+//4 .
+
+// alert("Welcome to JS Land...");
+// alert("Happy Coding");
+
+// 5.
+// alert("Hello... I can run JS through my web browser's console.");
+
+// Chapter 02 Assignment
+
+// 1. Declare a variable called username.
+
+// var username;
+
+// 2. Declare a variable called myName & assign to it a string that represents your Full Name.
+
+// var myName = "Muhammad Yasir Hussain";
+
+// 3. Write script to
+//      a) Declare a JS variable, titled message.
+//      b) Assign “Hello World” to variable message
+//      c) Display the message in alert box.
+
+// var message = "Hello World";
+// alert(message);
+
+// 4.
+
+// var myName = "Yasir";
+// var age = "27 years old";
+// var job = "Certified Mobile Application Developer";
+
+// alert(myName);
+// alert(age);
+// alert(job);
+
+// 5.
+
+// var food = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+// alert(food);
+
+// 6.
+
+// var email = "optimisticyasir@outlook.com";
+// alert("My Email address is "+email)
+
+// 7.
+
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book "+ book)
+
+// 8.
+
+// document.write("Yah! I can write HTML content through JavaScript")
+
+// 9.
+
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+// Chapter 03 Assignment
+
+// 01 .
+
+// var age = 27;
+// alert("I am "+age+" old");
+
+// 02.
+
+// var visited = 1;
+// alert("You have visited this site "+ visited++ +" times")
+// alert("You have visited this site "+ visited++ +" times")
+// alert("You have visited this site "+ visited++ +" times")
+
+// 03. 
+
+// var birthYear = 1995;
+// alert("My birth year is " + birthYear +"\nData type of my declared variable is number");
+
+// 04.
+
+    // var visitorName = prompt("Your Name");
+    // var productTitle = prompt("Product Title");
+    // var quantity = prompt("How many products you wants to order");
+    // var store = "www.xyzClothing.com";
+
+    // document.write(visitorName + " ordered "+quantity+"-"+ productTitle+" on "+ store);
+
+// Chapter 04 Assignment
