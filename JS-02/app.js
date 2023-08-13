5f912cd94bd38f5735e798236e12df5f9152149a
Muhammad Yasir <optimisticyasir@outlook.com>
2023-08-14 02:31:02 +0500

    updated JS-2 Assignment

--- JS-02/app.js
+++ JS-02/app.js
@@ -91,14 +91,227 @@ does not enter a new number, multiplication table of 5 should be displayed by de
     e) Now calculate total marks and percentage and show the
     result in browser like this.(Hint: user table)
 */
-var sub_1 = prompt("Enter your first Subject");
-var sub_2 = prompt("Enter your Second Subject");
-var sub_3 = prompt("Enter your Third Subject");
-var totalMarks = 100;
-var obtainMarksSub_1 = prompt("Enter your first Subject Marks");
-var obtainMarksSub_2 = prompt("Enter your Second Subject Marks");
-var obtainMarksSub_3 = prompt("Enter your Third Subject Marks");
+// var sub_1 = prompt("Enter your first Subject");
+// var sub_2 = prompt("Enter your Second Subject");
+// var sub_3 = prompt("Enter your Third Subject");
+// var totalMarks = 100;
+// var totalMarksAllSubject= 100*3;
+// var obtainMarksSub_1 = +prompt("Enter your first Subject Marks");
+// var obtainMarksSub_2 = +prompt("Enter your Second Subject Marks");
+// var obtainMarksSub_3 = +prompt("Enter your Third Subject Marks");
+// totalObtainMarks = obtainMarksSub_1+obtainMarksSub_2+obtainMarksSub_3;
 
-document.write("<table><tr><th>Subject</th><th>Total Marks</th><th>Obtained Marks</th><th>Percentage</th></tr> <br /><tr><td>"+sub_1+"</td> <td>"+ totalMarks+"</td></td"+ obtainMarksSub_1+"</td>"+"</tr></table>")
+// var percentageOfFistSubject = obtainMarksSub_1 *100 /totalMarks;
+// var percentageOfSecondSubject = obtainMarksSub_2 *100 /totalMarks;
+// var percentageOfThirdSubject = obtainMarksSub_3 *100 /totalMarks;
+
+// var overAllPercentage = totalObtainMarks *100 /totalMarksAllSubject;
 
 
+// document.write("<table><tr><th>Subject</th><th>Total Marks</th><th>Obtained Marks</th><th>Percentage</th></tr> <br /><tr><td>"+sub_1+"</td> <td>"+ totalMarks+"</td><td>"+ obtainMarksSub_1+"</td><td>"+ percentageOfFistSubject+"% </td>"+"</tr>"
+// +"<tr><td>"+sub_2+"</td> <td>"+ totalMarks+"</td><td>"+ obtainMarksSub_2+"</td><td>"+ percentageOfSecondSubject+"% </td>"+"</tr>"+
+// "<tr><td>"+sub_3+"</td> <td>"+ totalMarks+"</td><td>"+ obtainMarksSub_3+"</td><td>"+ percentageOfThirdSubject+"% </td>"+"</tr>"+
+// "<tr><th></th><th>"+ totalMarksAllSubject+"</th><th>"+ totalObtainMarks+"</th><th>"+ overAllPercentage+"% </th>"+"</tr></table>")
+
+
+
+// ====================== ASSIGNMENT CHAPTER 9-11 ==========================
+
+//1. Write a program to take “city” name as input from user. If user enters “Karachi”, welcome the user like this:“Welcome to city of lights”
+
+// var city = prompt("Where do you live?")
+// if(city === "karachi"){
+//     alert("Welcome to city of lights")
+// }else(
+//     alert("Welcome!")
+// )
+
+// 2. Write a program to take “gender” as input from user. If the user is male, give the message: Good Morning Sir. If the user is female, give the message: Good Morning Ma’am.
+
+// var gender = prompt("Tell us your Gender");
+// var message = "";
+
+// if (gender === "male"){
+//     message = "Good Morning Sir";
+// }
+// if(gender === "female"){
+//     message = "Good Morning Ma'am"
+// }
+// alert(message);
+
+// 3. Write a program to take input color of road traffic signal from the user & show the message according to this table:
+
+// var signalColor = prompt("Tell us Road Traffic Signal Color");
+// var signal = "";
+
+// if (signalColor === "red"){
+//     signal = "Must Stop";
+// }
+// else if(signalColor === "yellow"){
+//     signal = "Ready to move"
+// }
+// else if(signalColor === "green"){
+//     signal = "Move now"
+// }
+// alert(signal);
+
+// 4. Write a program to take input remaining fuel in car (in litres) from user. If the current fuel is less than 0.25litres, show the message “Please refill the fuel in your car”
+
+// var fuel = +prompt("Fuel Remaining in Your Tank in Litres");
+// if (fuel< 0.25){
+//     alert("Please refill the fuel in your car")
+// }
+
+// 5. Run this script, & check whether alert message would be displayed or not. Record the outputs.
+
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+// }
+
+// ====QUESTION 6=======
+
+// var obatinMarks1 = +prompt("Enter your first subject Marks")
+// var obatinMarks2 = +prompt("Enter your Second subject Marks")
+// var obatinMarks3 = +prompt("Enter your Third subject Marks")
+// var totalMarks = +prompt("Enter Total Marks")
+// var totalMarksObtained = obatinMarks1+obatinMarks2+obatinMarks3;
+// var totalPercentage = totalMarksObtained*100 / totalMarks;
+// message = "";
+// if(totalPercentage >= 80){
+//     document.write("<h1>Marks Sheet</h1> <p>Total Marks : "+totalMarks+"</p>"+
+//     "<p>Marks Obtained : "+totalMarksObtained+"</p>"+
+//     "<p>Percentage : "+totalPercentage+"%</p>"+
+//     "<p>Grade: A-one</p><p>Remarks : Excellent</p>") 
+// }else if (totalPercentage >= 70){
+//     document.write("<h1>Marks Sheet</h1> <p>Total Marks : "+totalMarks+"</p>"+
+//     "<p>Marks Obtained : "+totalMarksObtained+"</p>"+
+//     "<p>Percentage : "+totalPercentage+"%</p>"+
+//     "<p>Grade: A</p><p>Remarks : Good</p>") 
+// }else if (totalPercentage >= 60){
+//     document.write("<h1>Marks Sheet</h1> <p>Total Marks : "+totalMarks+"</p>"+
+//     "<p>Marks Obtained : "+totalMarksObtained+"</p>"+
+//     "<p>Percentage : "+totalPercentage+"%</p>"+
+//     "<p>Grade: B</p><p>Remarks : You need to Improve</p>") 
+// }
+// else if (totalPercentage < 60){
+//     document.write("<h1>Marks Sheet</h1> <p>Total Marks : "+totalMarks+"</p>"+
+//     "<p>Marks Obtained : "+totalMarksObtained+"</p>"+
+//     "<p>Percentage : "+totalPercentage+"%</p>"+
+//     "<p>Grade: Fail</p><p>Remarks : Sorry</p>") 
+// }else{
+//     alert("Incorrect Information given")
+// }
+
+// =====QUESTION 7=========
+
+// var num = 7;
+// var input = +prompt("Guess a number between 1 to 10");
+// if(num === input){
+//     alert("Bingo! Correct answer")
+// }else if (num < ++input){
+//     alert("Close enough to the correct answer.")
+// }else{
+//     alert("Try Again")
+// }
+
+// =====QUESTION 8. Write a program to check whether the given number is divisible by 3. Show the message to the user if the number is divisible by 3.
+
+// var num =9;
+// var answer = +prompt("Enter your number to check divisible by 3");
+// var remainder = num % answer
+
+// if ((remainder === 0)){
+//     alert("Number is divisible by 3")
+// }
+// else {
+//     alert("Number is not visible by num")
+// }
+
+// QUESTION 9. Write a program that checks whether the given input is an even number or an odd number.
+
+// var inputNum = +prompt("Enter your number to check it's even or odd");
+// inputRemainder = inputNum % 2;
+
+// if (inputRemainder === 0){
+//     alert("It's even number")
+// }else{
+//     alert("It's an odd number")
+// }
+
+// ======= QUESTION 10.
+
+// var temperature = +prompt("Enter Temperature in your area");
+
+// if (temperature >40){
+//     alert("It is too hot outside")
+// }
+// else if (temperature >30){
+//     alert("The Weather today is Normal.")
+// }
+// else if (temperature >20){
+//     alert("Today’s Weather is cool.")
+// }
+// else if (temperature >10){
+//     alert("OMG! Today’s weather is so Cool.")
+// }
+// else{
+//     alert("Enter correct temperature")
+// }
+
+// ===========  QUESTION 11
+
+/* var num1 = +prompt("Enter your first number");
+var operator = prompt("Type your operation to perform")
+var num2 = +prompt("Enter your Second number");
+var totalAnswer = "";
+
+if (operator === "+"){
+    totalAnswer = num1 +num2
+}
+else if(operator === "-"){
+    totalAnswer = num1 - num2
+}
+else if(operator === "*"){
+    totalAnswer = num1 * num2
+}
+else if(operator === "/"){
+    totalAnswer = num1 / num2
+}
+else if(operator === "%"){
+    totalAnswer = num1 % num2
+}
+alert(totalAnswer)
+*/
