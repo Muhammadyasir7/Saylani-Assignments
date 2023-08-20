e189a61f43ea6d201f773c120d2be8e3d94c1d42
Muhammad Yasir <optimisticyasir@outlook.com>
2023-08-20 04:22:30 -0700

    added new assignment

---
+++ JS-03/app.js
@@ -0,0 +1,35 @@
+// ========================== CHAPTER 14-16 =================================
+
+// Q :1. Declare an empty array using JS literal notation to store student names in future.
+
+// var studentName = [];
+
+
+// Q 2. Declare an empty array using JS object notation to store student names in future.
+
+// var students = new [];
+
+// Q3. Declare and initialize a strings array.
+
+// var myArr = ["Myself Muhammad Yasir"];
+
+// Q4. Declare and initialize a numbers array.
+
+// var count = [22]
+
+// Q4. Declare and initialize a boolean array.
+
+// var arr = [true,false];
+
+// 6. Declare and initialize a mixed array.
+
+//var mixArr = ["Muhammad Yasir", 27, true];
+
+// Q7. Declare and Initialize an array and store available education qualifications in Pakistan (e.g. SSC, HSC, BCS,BS, BCOM, MS, M. Phil., PhD). Show the listed qualifications in your browser like:
+
+//var eduQualification = ["SSC","HSC","BCS","BS","BCOM","MS","M.Phil.","PhD"];
+
+//document.write("<h1>Qualifications</h1> <h3>1)"+eduQualification[0]+"</h3><h3>2)"+eduQualification[1]+"</h3><h3>3)"+eduQualification[2]+"</h3><h3>4)"+eduQualification[3]+"</h3><h3>5)"+eduQualification[4]+"</h3><h3>6)"+eduQualification[5]+"</h3><h3>7)"+eduQualification[6]+"</h3><h3>8)"+eduQualification[7]+"</h3>")
+
+
+// 8. Write a program to store 3 student names in an array.Take another array to store score of these three students. Assume that total marks are 500 for each student, display the scores & percentages of students like:
