58b188adca58cfc4b8638217141b15db0dc10bd8
Muhammad Yasir <optimisticyasir@outlook.com>
2023-08-31 23:55:48 +0500

    updated Assignment

--- JS-04/app.js
+++ JS-04/app.js
@@ -115,3 +115,21 @@ document.write("The Smallest number is"+smallestNum); */
 // 10. Write a program to print multiples of 5 ranging 1 to 100.
 
 
+// ================= Chapter 21-25 =================
+
+// 1. Write a program that takes two user inputs for first and last name using prompt and merge them in a new variable titled fullName. Greet the user using his full name.
+
+var firstName = prompt("Enter your first name")
+var lastName = prompt("Enter your last name");
+
+var fullName = firstName+" "+lastName;
+
+alert("Hello "+ fullName)
+
+// 2. Write a program to take a user input about his favorite mobile phone model. Find and display the length of user input in your browser;
+
+var mobileModel = prompt("Enter your favorite mobile phone model");
+
+var lengthOfModel= mobileModel.length;
+
+alert(lengthOfModel)
