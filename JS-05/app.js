48953e5f76faf5e873f146e2576ef68db8fcdc5a
Muhammad Yasir <optimisticyasir@outlook.com>
2023-09-09 23:55:50 +0500

    added new assignment

---
+++ JS-05/app.js
@@ -0,0 +1,31 @@
+// ==================Chapter 26-30================
+
+/*====Q1. Write a program that takes a positive integer from user & display the following in your browser.
+    a. number
+    b. round off value of the number
+    c. floor value of the number
+    d. ceil value of the number
+*/
+
+/*var userInput = parseFloat(prompt("Enter any number in points"));
+
+document.write(userInput +"<br />")
+document.write("Round off Value: " +Math.round(userInput) +"<br />")
+document.write("Floor Value: "+ Math.floor(userInput) +"<br />")
+document.write("Ceil Value: "+ Math.ceil(userInput) +"<br />")
+*/
+
+/* =====Q2.Write a program that takes a negative floating point number from user & display the following in your browser.
+    a. number
+    b. round off value of the number
+    c. floor value of the number
+    d. ceil value of the number
+*/
+var userInput = parseFloat(prompt("Enter any negative floating number in points"));
+
+document.write(userInput +"<br />")
+document.write("Round off Value of the number: " +Math.round(userInput) +"<br />")
+document.write("Floor Value of the number: "+ Math.floor(userInput) +"<br />")
+document.write("Ceil Value of the number: "+ Math.ceil(userInput) +"<br />")
+
+
