95e059fd94aef0880e42fea8d02faff999bfac14
Muhammad Yasir <optimisticyasir@outlook.com>
2023-09-10 01:10:03 +0500

    updated JS-05 assignment

--- JS-05/app.js
+++ JS-05/app.js
@@ -21,11 +21,64 @@ document.write("Ceil Value: "+ Math.ceil(userInput) +"<br />")
     c. floor value of the number
     d. ceil value of the number
 */
-var userInput = parseFloat(prompt("Enter any negative floating number in points"));
+/*var userInput = parseFloat(prompt("Enter any negative floating number in points"));
 
 document.write(userInput +"<br />")
 document.write("Round off Value of the number: " +Math.round(userInput) +"<br />")
 document.write("Floor Value of the number: "+ Math.floor(userInput) +"<br />")
 document.write("Ceil Value of the number: "+ Math.ceil(userInput) +"<br />")
+*/
+
+/* 3. Write a program that displays the absolute value of a number. E.g. absolute value of -4 is 4 & absolute value of 5 is 5
+
+var absNum = parseFloat(prompt("Enter any number to check absoulute number."))
+
+document.write("The absolute value of "+absNum+" is "+Math.abs(absNum)) */
+
+// 4. Write a program that simulates a dice using random() method of JS Math class. Display the value of dice in your browser.:
+
+// var num = 6;
+// for (var i = 1; i <= 2 ;i++){
+//     document.write("random dice Value: "+Math.ceil(Math.random()*6)+"<br />")
+// }
+
+// 5. Write a program that simulates a coin toss using random() method of JS Math class. Display the value of coin in your browser
+
+/* var num = Math.ceil(Math.random()*2);
+
+if(num == 1){
+    document.write("<h1>"+num + "<br />random coin value: Heads")
+}else{
+    document.write("<h1>"+num + "<br />random coin value: Tails")
+}
+*/
+// 6. Write a program that shows a random number between 1 and 100 in your browser.
+
+// var numInput = Math.round(Math.random()*100)
+// document.write("<h1>random number between 1 and 100: " + numInput)
+
+/*7. Write a program that asks the user about his weight. Parse the user input and display his weight in your browser.Possible user inputs can be:
+    a. 50
+    b. 50kgs
+    c. 50.2kgs
+    d. 50.2kilograms
+*/
+
+// var userInput = parseFloat(prompt("Enter your weight in kilograms"));
+// document.write("The weight of user is "+userInput+" kilograms")
+
+// 8. Write a program that stores a random secret number from 1 to 10 in a variable. Ask the user to input a number between 1 and 10. If the user input equals the secret number, congratulate the user.
+
+// var secretNum = Math.ceil(Math.random()*10);
+// var userNum = Number(prompt("Enter a number between 1 and 10"));
+
+// if(secretNum == userNum){
+//     alert("Congratulations ! You guessed right")
+// }else{
+//     alert("Try Again!")
+// }
+
+// =====================CHAPTER 31-34=================================
+
 
 
