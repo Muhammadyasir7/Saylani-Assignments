be6b417a9896a9cf8c6c73d127691799dd2fe252
Muhammad Yasir <optimisticyasir@outlook.com>
2023-09-18 20:26:10 +0500

    update JS Calculator

--- JS Calculator/app.js
+++ JS Calculator/app.js
@@ -1,4 +1,27 @@
+var inputField= document.getElementById('input2');
 
-function abc(){
-    document.getElementById('input2').value +=document.getElementById('num-val').value;
+function abc(value){
+    inputField.value += value
+    return inputField
 }
+
+function emptyInput(){
+    inputField.value = "";
+}
+function operation(){
+    var last = inputField.value[inputField.value.length -1]
+    var arr = ["+","-","*","/"]
+    if(arr.indexOf(last) !== -1){
+        inputField.value = inputField.value.slice(0, -1);
+    }else{
+        var x = document.getElementById('input2').value
+        var y = eval(x);
+        document.getElementById('input2').value = y
+        return y
+    }
+}
+
+// function lastRemove(){
+//     var newField = inputField.value.lastIndexOf();
+//     newField = inputField.value
+// }
