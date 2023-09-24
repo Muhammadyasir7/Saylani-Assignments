2b04b5aa5e7ae00a2b296a63a69ace8d71433a5b
Muhammad Yasir <optimisticyasir@outlook.com>
2023-09-24 21:35:36 +0500

    updated JS-08

--- JS-08(GameClone)/index.js
+++ JS-08(GameClone)/index.js
@@ -1,2 +1,20 @@
-var character1 = document.getElementById('character1');
-
+var forward = 0;
+var right = 0;
+function abc(){
+    var ioriCharacter = document.getElementById('character1');
+    console.log(event.keyCode)
+    if(event.keyCode === 39){
+        forward = forward + 10;
+        ioriCharacter.style.left = forward + 'px';
+        ioriCharacter.src = 'images/iori-xi-walk2.gif'
+        setTimeout(function(){
+            
+        })
+    }
+    if(event.keyCode === 37){
+        forward = forward - 10;
+        ioriCharacter.style.left = forward + 'px';
+        ioriCharacter.src = 'images/iori-xi-walk2.gif'
+    }
+}
+window.onkeydown = abc;
