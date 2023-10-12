255fb08c7ebb254dd619a0ee18447ce2dad9818b
Muhammad Yasir <optimisticyasir@outlook.com>
2023-10-12 21:16:51 +0500

    updated JS-09

--- JS-09/index.js
+++ JS-09/index.js
@@ -1,4 +1,5 @@
 var backImg;
+var textColor;  
 
 function img2(src){
     backImg = src;
@@ -6,24 +7,40 @@ function img2(src){
     text.style.backgroundImage = 'url(' + backImg + ')'
 }
 
+function textCol(){
+    textColor = document.getElementById('text-col').value;
+    document.getElementById('text').style.color = textColor;
+    return textColor;
+}
+
 function post(){
     var sharedWith = document.getElementById('share').value;
-    var text = document.getElementById('text').value;
-    if(text === ""){
+    var text = document.getElementById('text');
+    if(text.value === ""){
         sweetAlert("Oops...", "Something went wrong!", "error");
     }
     else{
-    document.getElementById('main2').style.display ="block";
-    document.getElementById('shared-with').innerHTML =sharedWith;
-    document.getElementById('text-sec').innerHTML = text;
+        var main3 = document.getElementById('main3')
+        main3.innerHTML += `
+    <div class="main main1" id="main1">
+        <h1>Your post</h1> <hr>
+        <div class="main-profile">
+            <img src="images/yasir-removebg-preview.png" width="50px" class="profile-pic">
+            <span>
+                <p>Muhammad Yasir</p>
+                <p id="shared-with">${sharedWith}</p>
+            </span>
+        </div>
+        <div class="textarea-inp" id="text-sec" style="background-image: url(${backImg});background-size:cover;background-repeat:no-repeat;color:${textColor}">${text.value}
+        </div>
+        <button onclick="returnBtn()" id="retrn">Delete this Post</button>
+    </div>
+    `
+    text.value = ""
+    document.getElementById('text').style.background = "none"
+}
+}
 
-    var textDiv = document.getElementById('text-sec');
-    textDiv.style.backgroundImage= 'url(' + backImg + ')'
-    textDiv.style.backgroundRepeat = 'no-repeat';
-    textDiv.style.backgroundSize = 'cover';
-;
-}
-}
 function returnBtn(){
     var returnId = document.getElementById('retrn')
     var noneBtn = document.getElementById('main2').style.display ="none";
