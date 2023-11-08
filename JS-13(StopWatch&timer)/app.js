91d69598ae58a77c6201d8567cdba8e4d1e36a1b
Muhammad Yasir <optimisticyasir@outlook.com>
2023-11-08 21:41:38 +0500

    updated JS-13

---
+++ JS-13(StopWatch&timer)/app.js
@@ -0,0 +1,92 @@
+var main = document.getElementById("mainDiv");
+var sec =document.getElementById("sec");
+var hour = 0;
+var min = 0;
+var sec = 0;
+var btnStart = document.getElementById("startBtn");
+
+var timer = false
+var count = 0;
+
+function start(){
+    timer = true;
+    stopwatch()
+    btnStart.setAttribute("disabled","disabled")
+}
+
+function stop(){
+    timer =false;
+    btnStart.removeAttribute("disabled")
+}
+function reset(){
+    sec =0;
+    min = 0;
+    count = 0;
+    hour = 0
+    var timeNum = document.getElementById("timeNum")
+    timeNum.innerHTML = count;
+    var second = document.getElementById('sec')
+    second.innerHTML = sec;
+    var mints= document.getElementById("min");
+    mints.innerHTML = min
+    var hourss= document.getElementById("hourss");
+    hourss.innerHTML = hour;
+    // clearInterval(start)
+}
+
+function stopwatch(){
+    if(timer == true){
+        count += 1;
+        var timeNum = document.getElementById("timeNum")
+        timeNum.innerHTML = count
+        if(count == 99){
+            count = 0;
+            var second = document.getElementById('sec')
+            sec += 1;
+            second.innerHTML = sec;
+        }
+
+        if(sec == 59 && count == 59){
+            count = 0;
+            sec = 0;
+
+            var mints= document.getElementById("min");
+            min += 1;
+            mints.innerHTML = min
+        }
+        if(min == 59 && sec == 59 && count == 59){
+            count = 0;
+            sec = 0;
+            min = 0;
+
+            var hourss= document.getElementById("hourss");
+            hour += 1;
+            hourss.innerHTML = hour;
+
+        }
+        
+    }
+    setTimeout("stopwatch()",10)
+}
+
+var stopDiv = document.getElementById("stopwatch");
+stopDiv.addEventListener("click",function(){
+    document.getElementById("mainDiv").innerHTML = `
+            <span class="num" id="hourss">0</span>
+            <span>H</span>
+            <span class="num" id="min">0</span>
+            <span>M</span>
+            <span class="num" id="sec">00</span>
+            <span>S</span>
+            <span class="num" id="timeNum">00</span>
+    `
+})
+
+function timers(){
+    document.getElementById("mainDiv").innerHTML = `
+            <span class="num" id="hourss">0</span>
+            <span>H</span>
+            <span class="num" id="min">0</span>
+            <span>M</span>
+    `
+}
